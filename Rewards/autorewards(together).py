3b5c232d724f794b0c1cf8aac4f8250ad019c12c
GeekElement <verificati0nc0de@outlook.com>
2024-11-09 14:39:10 +0800

    Update autorewards(together).py

--- Rewards/autorewards(together).py
+++ Rewards/autorewards(together).py
@@ -1,32 +1,33 @@
-import webbrowser
 import random
 import time
 import pyautogui
-import os
+import subprocess
 
-# 注册 Edge 浏览器
+# 定义 Edge 和 Chrome 的路径
 edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
-
-# 注册 Chrome 浏览器
 chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
-# 重复edge搜索的次数
+# Bing 搜索引擎的 URL
+bing_url = "https://www.bing.com"
+
+# 重复 Edge 打开次数
 repeat_count_edge = 30  # 根据需要设置次数
 
-# 重复chrome搜索的次数
+# 重复 Chrome 打开次数
 repeat_count_chrome = 10  # 根据需要设置次数
 
 for _ in range(repeat_count_edge):
+    # 使用 Edge 浏览器打开
+    subprocess.Popen(edge_path)  # 直接通过命令行打开浏览器
+
+    # 等待 Edge 打开
+    time.sleep(2)  # 根据需要调整时间
+
     # 生成一个六位数的随机数
     random_number = random.randint(100000, 999999)
 
-    # 使用 Edge 浏览器打开 Bing
-    webbrowser.get('edge').open('https://www.bing.com')
-
-    # 等待 Edge 打开
-    time.sleep(3)  # 根据需要调整时间
+    # 等待一段时间，确保浏览器完全加载
+    time.sleep(1)  # 等待浏览器完全加载
 
     # 在输入框中输入随机数
     pyautogui.typewrite(str(random_number))
@@ -38,25 +39,26 @@ for _ in range(repeat_count_edge):
     time.sleep(3)  # 根据需要调整时间
 
     # 关闭 Edge 浏览器窗口
-    os.system("taskkill /IM msedge.exe /F")
+    subprocess.call(["taskkill", "/IM", "msedge.exe", "/F"])
     
     # 等待任务被彻底关闭
-    time.sleep(3)
-    
+    time.sleep(3)  # 增加等待时间以确保浏览器完全关闭
 
 # 切换浏览器间隙    
-time.sleep(3)
-
+time.sleep(5)
 
 for _ in range(repeat_count_chrome):
-    # 生成一个六位数的随机数
-    random_number = random.randint(100000, 999999)
-
     # 使用 Chrome 浏览器打开 Bing
-    webbrowser.get('chrome').open('https://www.bing.com')
+    subprocess.Popen([chrome_path, bing_url])  # 直接通过命令行打开浏览器并指定 URL
 
     # 等待 Chrome 打开
     time.sleep(3)  # 根据需要调整时间
+
+    # 生成一个六位数的随机数
+    random_number = random.randint(100000, 999999)
+
+    # 等待一段时间，确保浏览器完全加载
+    time.sleep(1)  # 等待浏览器完全加载
 
     # 在输入框中输入随机数
     pyautogui.typewrite(str(random_number))
@@ -68,7 +70,8 @@ for _ in range(repeat_count_chrome):
     time.sleep(3)  # 根据需要调整时间
 
     # 关闭 Chrome 浏览器窗口
-    os.system("taskkill /IM chrome.exe /F")
+    subprocess.call(["taskkill", "/IM", "chrome.exe", "/F"])
     
     # 等待任务被彻底关闭
-    time.sleep(3)
+    time.sleep(5)  # 增加等待时间以确保浏览器完全关闭
+
